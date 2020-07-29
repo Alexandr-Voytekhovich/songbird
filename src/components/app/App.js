b46d0dce0e7b35cfcef3abe86c67cbc31fcf05f5
Alexandr Voytekhovich <avth.tec@gmail.com>
2020-07-29 23:07:12 +0300

    feat: add initial dependencies

---
+++ src/components/app/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom'
+
+import './common.scss'
+import './constants.scss'
+import './media.scss'
+
+const el = <h1>Songbird</h1>
+
+ReactDom.render(el, document.getElementById('root'))
